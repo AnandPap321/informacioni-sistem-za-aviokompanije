7243212458a672999be867b0e6f85bd808ec1738
AnandPap321 <apap1@etf.unsa.ba>
2025-05-28 15:15:17 +0200

    Prepravljen URL atribut axios instance.

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const api = axios.create({
-  backendUrl,
-  withCredentials: true,
+  baseURL: backendUrl,
+  // withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
