e6a98b539f60ca96256b704b3dcc2efcba9d60e0
AnandPap <anandpap@live.com>
2025-05-28 15:22:55 +0200

    Prepravljen nacin autorizacije.

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(compress());
-app.use(cors({ credentials: true }));
+app.use(cors());
 
 app.use("/api/korisnici", userRute);
 app.use("/api/admin", adminRute);
